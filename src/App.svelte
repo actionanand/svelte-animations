5cbfa4beba966011d8353c773070467425666540
Anand raja <46064269+actionanand@users.noreply.github.com>
2022-01-27 23:25:32 +0530

    tweened

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script>
+  import progress from './app-store';
+
   export let appName;
 </script>
 
 <div class="title-warap">
   <h1 class="capitalize-it">{appName}</h1>
 </div>
+
+<progress value="{$progress}"></progress>
 
 <style>
   .title-warap {
